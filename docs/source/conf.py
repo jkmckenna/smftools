9daae5cdef431880ff84c910751b13f9892fe827
Joey McKenna <95374311+jkmckenna@users.noreply.github.com>
2024-09-01 16:33:57 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,7 @@ project = 'smftools'
 copyright = '2024, Joseph McKenna'
 author = 'Joseph McKenna'
 release = '0.1.0'
+repository_url = 'https://github.com/jkmckenna/smftools'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -19,10 +20,28 @@ extensions = ["myst_nb"]
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+html_context = {
+    "display_github": True,
+    "github_user": "jkmckenna",
+    "github_repo": project,
+    "github_version": "main",
+    "conf_py_path": "/docs/source/",
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_book_theme"
+html_title = project
+
+html_theme_options = {
+    "repository_url": repository_url,
+    "use_repository_button": True,
+    "logo_only": True,
+    "show_toc_level": 1,
+    "path_to_docs": "docs/",
+    "repository_branch": release,
+}
+
 html_static_path = ['_static']
+
