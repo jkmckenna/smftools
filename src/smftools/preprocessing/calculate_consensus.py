e2b2b5beb6940ce5940d4ecc703de588c4a8c370
jkmckenna <jkmckenna@berkeley.edu>
2024-09-13 10:43:05 -0700

    readme

---
+++ src/smftools/preprocessing/calculate_consensus.py
@@ -0,0 +1,15 @@
+# calculate_consensus
+
+def calculate_consensus(adata, reference_column, sample_column):
+    """
+    Takes an input AnnData object, the reference to subset on, and the sample name to subset on to calculate the consensus sequence of the read set.
+
+    Parameters:
+        adata (AnnData):
+        reference_column (str):
+        sample_column (str):
+
+    Returns:
+        None
+    
+    """
