1870620a567cf7cba4d1a45c7bbf079311e89bb1
jkmckenna <jkmckenna@berkeley.edu>
2026-01-14 09:27:29 -0800

    formating

--- src/smftools/cli/preprocess_adata.py
+++ src/smftools/cli/preprocess_adata.py
@@ -218,6 +218,7 @@ def preprocess_adata_core(
     """
     from pathlib import Path
 
+    from ..metadata import record_smftools_metadata
     from ..plotting import plot_read_qc_histograms
     from ..preprocessing import (
         append_base_context,
@@ -234,7 +235,6 @@ def preprocess_adata_core(
         flag_duplicate_reads,
         load_sample_sheet,
     )
-    from ..metadata import record_smftools_metadata
     from ..readwrite import make_dirs
     from .helpers import write_gz_h5ad
 
